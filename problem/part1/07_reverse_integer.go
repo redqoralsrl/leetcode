d07559159ba125f7140b14f359a88e5e50427105
redqoralsrl <redqoralsrl@gmail.com>
2024-10-06 20:38:43 +0900

    feat description

--- problem/part1/07_reverse_integer.go
+++ problem/part1/07_reverse_integer.go
@@ -1,6 +1,33 @@
 package main
 
-import "strconv"
+/**
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+
+Constraints:
+
+-231 <= x <= 231 - 1 // 31승임
+
+*/
+
+import (
+	"math"
+	"strconv"
+)
 
 func reverse(x int) int {
 	stringInt := strconv.Itoa(x)
@@ -17,7 +44,11 @@ func reverse(x int) int {
 		}
 	}
 
-	result, _ := strconv.Atoi(result)
+	res, _ := strconv.Atoi(result)
 
-	return 0
+	if res >= math.MinInt32 && res <= math.MaxInt32 {
+		return res
+	} else {
+		return 0
+	}
 }
